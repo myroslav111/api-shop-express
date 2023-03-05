36af4c6aeb3e76fbafe72c3ad8e3c697d1f02c3b
myroslav <myrkozar@gmail.com>
2023-03-05 12:09:47 +0200

    init project

---
+++ app.js
@@ -0,0 +1,52 @@
+/** for work with server */
+const express = require('express');
+/** log event  */
+const logger = require('morgan');
+/** it helps us  */
+const cors = require('cors');
+/** library to work with env. files */
+require('dotenv').config();
+
+/** init express */
+const app = express();
+
+/** starting brake point for work with contacts */
+// ?const contactsRouter = require('./routes/api/contacts');
+
+/** starting brake point for work with users */
+// ?const authRouter = require('./routes/api/auth');
+
+/** виводить в консоль статус операції (200 і можливо ще якась інфо) */
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+app.use(logger(formatsLogger));
+
+/** this middleware helps solve problems related to cors */
+app.use(cors());
+
+/** this middleware recognizes content type of body */
+app.use(express.json());
+
+/** midl access to get file from public without extention by frontend */
+// ?app.use(express.static('public'));
+/** here are send our routes */
+// ?app.use('/api/contacts', contactsRouter);
+// ?app.use('/api/users', authRouter);
+
+const getAllContacts = async (req, res) => {
+  res.status(200).json({ message: 'we got it' });
+};
+
+app.use('/api/products', getAllContacts);
+
+/** if front-end require for what is not */
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' });
+});
+
+/** if something went wrong */
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Server error' } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,21 @@
+/** import app there are our logic */
+const app = require('./app');
+/** for work with db */
+const mongoose = require('mongoose');
+
+const { DB_HOST_CAFE, PORT = 3000 } = process.env;
+/** when db exist next step to run server */
+mongoose
+  .connect(DB_HOST_CAFE)
+  .then(() => {
+    console.log('Server running. Use our API on port: 4200');
+    app.listen(PORT);
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log('Server running. Use our API on port: 3000');
+// });
